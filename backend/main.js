33cda94cf8369fbe89440a426d2c086b8acd01ae
Sean Z <contact@seanzeng.com>
2019-08-20 23:20:46 +1200

    Item edit

--- backend/main.js
+++ backend/main.js
@@ -10,30 +10,41 @@ const calculations = [
         buy: [I(G.Mining_and_smithing, 'Banite ore', 2)],
         sell: I(G.Mining_and_smithing, 'Bane bar')
     },
-    {
-        buy: [I(G.Mining_and_smithing, 'Rune bar')],
-        sell: I(G.Arrows, 'Rune arrow', 75)
-    },
+
     {
         buy: [I(G.Mining_and_smithing, 'Runite'), I(G.Mining_and_smithing,'Luminite')],
         sell: I(G.Mining_and_smithing, 'Rune bar')
     },
+
     {
-        buy: [I(G.Crafting_materials, 'Blue dragonhide')],
-        sell: I(G.Crafting_materials, 'Blue dragon leather')
+        buy: [I(G.Mining_and_smithing, 'Drakolith'), I(G.Mining_and_smithing,'Orichalcite ore')],
+        sell: I(G.Mining_and_smithing, 'Orikalkum bar')
     },
+
     {
-        buy: [I(G.Crafting_materials, 'Black dragonhide')],
-        sell: I(G.Crafting_materials, 'Black dragon leather')
+        buy: [I(G.Mining_and_smithing, 'Necrite ore'), I(G.Mining_and_smithing,'Phasmatite')],
+        sell: I(G.Mining_and_smithing, 'Necronium bar')
     },
-    {
-        buy: [I(G.Crafting_materials, 'Green dragonhide')],
-        sell: I(G.Crafting_materials, 'Green dragon leather')
-    },
-    {
-        buy: [I(G.Crafting_materials, 'Red dragonhide')],
-        sell: I(G.Crafting_materials, 'Red dragon leather')
-    }
+    // {
+    //     buy: [I(G.Mining_and_smithing, 'Rune bar')],
+    //     sell: I(G.Arrows, 'Rune arrow', 75)
+    // },
+    // {
+    //     buy: [I(G.Crafting_materials, 'Blue dragonhide')],
+    //     sell: I(G.Crafting_materials, 'Blue dragon leather')
+    // },
+    // {
+    //     buy: [I(G.Crafting_materials, 'Black dragonhide')],
+    //     sell: I(G.Crafting_materials, 'Black dragon leather')
+    // },
+    // {
+    //     buy: [I(G.Crafting_materials, 'Green dragonhide')],
+    //     sell: I(G.Crafting_materials, 'Green dragon leather')
+    // },
+    // {
+    //     buy: [I(G.Crafting_materials, 'Red dragonhide')],
+    //     sell: I(G.Crafting_materials, 'Red dragon leather')
+    // }
 ];
 
 async function run() {
